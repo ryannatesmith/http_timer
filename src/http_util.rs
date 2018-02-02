549cfbd6bbab08656fcd41715111f925df91db7b
Ryan Nate Smith <rsmith@localhost.localdomain>
2018-02-01 18:25:30 -0800

    Pull http functionality into its own module

---
+++ src/http_util.rs
@@ -0,0 +1,17 @@
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
+
+use self::hyper::Client;
+use self::hyper_tls::HttpsConnector;
+use self::tokio_core::reactor::Core;
+
+pub fn test() {
+    let mut core  = Core::new().unwrap();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
+        .build(&core.handle());
+
+    let res = core.run(client.get("Https://hyper.rs".parse().unwrap())).unwrap();
+    assert_eq!(res.status(), self::hyper::Ok);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-extern crate hyper;
-extern crate hyper_tls;
-extern crate tokio_core;
-
-use hyper::Client;
-use hyper_tls::HttpsConnector;
-use tokio_core::reactor::Core;
+mod http_util;
 
 fn main() {
-    let mut core  = Core::new().unwrap();
-    let client = Client::configure()
-        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
-        .build(&core.handle());
-
-    let res = core.run(client.get("Https://hyper.rs".parse().unwrap())).unwrap();
-    assert_eq!(res.status(), ::hyper::Ok);
+    http_util::test();
 }
