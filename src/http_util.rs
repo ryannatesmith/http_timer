137d1aae980e5a03a708d3c1e35b365598ac7a69
Ryan Nate Smith <rsmith@localhost.localdomain>
2018-02-01 18:34:05 -0800

    Start work on building out the http util

--- src/http_util.rs
+++ src/http_util.rs
@@ -6,6 +6,9 @@ use self::hyper::Client;
 use self::hyper_tls::HttpsConnector;
 use self::tokio_core::reactor::Core;
 
+const HTTP: &str = "http://";
+const HTTPS: &str = "https://";
+
 pub fn test() {
     let mut core  = Core::new().unwrap();
     let client = Client::configure()
@@ -14,4 +17,9 @@ pub fn test() {
 
     let res = core.run(client.get("Https://hyper.rs".parse().unwrap())).unwrap();
     assert_eq!(res.status(), self::hyper::Ok);
+}
+
+struct Host {
+    protocol : String,
+    domain : String
 }
